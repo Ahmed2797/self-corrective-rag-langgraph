096b8e0da4ee0672116738093cd10c7e82788c14
Ahmed <tanvirahmed754575@gmail.com>
2026-09-04 11:08:51 +0200

    add constants

---
+++ src/constants/__init__.py
@@ -0,0 +1,7 @@
+CHUNK_SIZE = 200
+CHUNK_OVERLAP = 50
+K = 4
+INDEX_NAME_CACHE_MEMORY = "crag-semantic-answer-cache"
+INDEX_NAME = "self-corrective-rag-langgraph"
+IMAGE_DIR = "src/data/images"
+DOCUMENTS = ["src/data/AI_Engineer-page1.pdf", "src/data/Tanvir_Ahmed_Enterprise_Demo_RAG_Knowledge_Base.pdf", "src/data/Tanvir_Ahmed_RAG_Knowledge_Base.pdf"]
