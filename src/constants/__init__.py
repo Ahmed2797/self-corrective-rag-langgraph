9ae20c4f3af1a1fab019635952c18292eaf328c7
Ahmed <tanvirahmed754575@gmail.com>
2026-09-18 21:26:06 +0200

    project constains

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -5,3 +5,5 @@ INDEX_NAME_CACHE_MEMORY = "crag-semantic-answer-cache"
 INDEX_NAME = "self-corrective-rag-langgraph"
 IMAGE_DIR = "src/data/images"
 DOCUMENTS = ["src/data/AI_Engineer-page1.pdf", "src/data/Tanvir_Ahmed_Enterprise_Demo_RAG_Knowledge_Base.pdf", "src/data/Tanvir_Ahmed_RAG_Knowledge_Base.pdf"]
+CACHE_THRESHOLD = 0.90
+CACHE_VERSION = "self-rag-v1"
